0ad9f7c3eb4ba17b4dcc16cf988f0b8ba48bd050
Ted Chow <142191365+Syphon-X@users.noreply.github.com>
2023-11-24 17:08:35 -0800

    Add files via upload

---
+++ Course 2/course2_numpy.py
@@ -0,0 +1,25 @@
+
+# Introduction to NumPy / Multi-Function Arrays and Matrices
+# Vectorization enables operations to be performed on multiple components of a data object at the same time.
+
+import numpy as np  # np is aliasing
+
+list_a = [1, 2, 3]
+list_b = [2, 4, 6]
+
+array_a = np.array(list_a)
+array_b = np.array(list_b)
+
+result = array_a * array_b
+print(result)
+
+
+
+
+
+
+
+
+
+
+
